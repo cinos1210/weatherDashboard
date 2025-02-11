28f4317989d2b8c75016b2b07c5505331b17c821
RAFAELARTUROGUTIERRE <rafael.gutierrez4666@alumnos.udg.mx>
2025-02-11 04:49:23 -0600

    adding Backend and getting weather data

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import streamlit as st
+from tkinter import Place
 
-st.Page("main.py",title='Weather APP')
+import streamlit as st
+import plotly.express as px
+from Backend import get_data
 
 st.title('Weather forcast for the next days')
 place = st.text_input("Place: ")
@@ -8,4 +10,12 @@ days = st.slider('Forcast Days', min_value=1, max_value=5,
                  help="Select the number of forcasted days")
 
 options = st.selectbox('Select data to view', ("Temperature", "Sky"))
+
 st.subheader(f"{options} for the next {days} days in {place}")
+
+
+d, t = get_data(Place, days, options)
+
+
+figure = px.line(x=d, y=t, labels={"x":"Date","y": "Temperature(C)"})
+st.plotly_chart(figure)
